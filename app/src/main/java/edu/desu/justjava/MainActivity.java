7e296406e18732917e97b9349f23417da2427ed9
dsuappacademy <mrtj48@gmail.com>
2016-07-12 17:20:48 -0400

    Added if statements and name input

--- app/src/main/java/edu/desu/justjava/MainActivity.java
+++ app/src/main/java/edu/desu/justjava/MainActivity.java
@@ -3,7 +3,10 @@ package edu.desu.justjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -24,8 +27,12 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String message = createOrderSummary(price);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String name = editText.getText().toString();
+        CheckBox orderSummaryCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean whippedcream = orderSummaryCheckbox.isChecked();
+        int price = calculatePrice(whippedcream);
+        String message = createOrderSummary(price, whippedcream, name);
         displayMessage(message);
     }
     /**
@@ -33,9 +40,15 @@ public class MainActivity extends ActionBarActivity {
      *
      * //@param quantity is the number of cups of coffee ordered
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean whippedcream) {
+        int basePrice = 5;
+
+        if(whippedcream){
+            basePrice = basePrice + 1;
+        }
+        return quantity * basePrice;
     }
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -54,19 +67,32 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1;
         displayQuantity(quantity);
+
+        if (quantity==100){
+            Toast.makeText(MainActivity.this, "Quantity can not be more than 100", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            quantity=quantity+1;
+        }
     }
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1;
-        displayQuantity(quantity);
+        if (quantity == 1) {
+            Toast.makeText(MainActivity.this, "Quantity must be more than 1", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
+
+        }
     }
 
-    public String createOrderSummary(int number) {
-        String chris = "Name: Kaptain Kunal\nQuantity: "+ quantity + "\nTotal:"+ NumberFormat.getCurrencyInstance().format(number)+"\nThank you!";
+    public String createOrderSummary(int number ,boolean whippedcream, String name) {
+        String chris = "Name: Kaptain Kunal\n"+"Add whipped cream? "+ whippedcream+"\nQuantity:" + quantity + "\nTotal:"+ NumberFormat.getCurrencyInstance().format(number)+"\nThank you!";
         return chris;
 
     }
